87d52007936f2124351257abb3909402ec5005f6
Nina Christine Eckertz <ninaec@ntnu.no>
2025-12-02 12:20:57 +0100

    Adjust new icons for the tool bar and margin fix

--- chimagingtool/imaging-app/src/components/Toolbar.tsx
+++ chimagingtool/imaging-app/src/components/Toolbar.tsx
@@ -1,18 +1,34 @@
+
+import { Images, Search, ChartLine, Navigation, WandSparkles, Save, MessageCircle, RotateCcw, ZoomIn, ZoomOut, SquareDashed, MapPin, MapPinPlus, WaypointsIcon} from "lucide-react";
+
+
 export default function Toolbar() {
   return (
     <header className="toolbar">
       <div className="title">Projects ▸ The Old Man in Warnemünde</div>
       <div className="tools">
-        <button aria-label="Open">📂</button>
-        <button aria-label="Save">💾</button>
+        <button aria-label="Open"><Images size={20} /></button>
+        <button aria-label="Save"><Search size={20} /></button>
         <span className="divider" />
-        <button aria-label="Zoom In">＋</button>
-        <button aria-label="Zoom Out">－</button>
-        <button aria-label="Fit">⤢</button>
-        <span className="divider" />
-        <button aria-label="Brush">🖌️</button>
-        <button aria-label="Select">🖱️</button>
-        <button aria-label="Measure">📏</button>
+         <button aria-label="Rotate"><Navigation size={20} /></button>
+ <button aria-label="Rotate"><RotateCcw size={20} /></button>
+
+        <button aria-label="Zoom In"><ZoomIn size={20} /></button>
+        <button aria-label="Zoom Out"><ZoomOut size={20} /></button>
+               <span className="divider" />
+        
+        <button aria-label="SelectSinglePoint"><MapPin size={20} /></button>
+<button aria-label="SelectMultiplePoints"><MapPinPlus size={20} /></button>
+<button aria-label="SelectShape"><SquareDashed size={20} /></button>
+<button aria-label="SelectLine"><WaypointsIcon size={20} /></button>
+
+   <span className="divider" />
+  <button aria-label="Rotate"><WandSparkles size={20} /></button>
+ <button aria-label="Rotate"><ChartLine size={20} /></button>
+   <span className="divider" />
+  <button aria-label="Rotate"><MessageCircle size={20} /></button>
+ <button aria-label="Rotate"><Save size={20} /></button>
+
       </div>
     </header>
   )
